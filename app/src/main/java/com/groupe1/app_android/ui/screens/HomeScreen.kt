3b49ad5275136a4eea9aa2ccdf81deccf4524354
Sagbyy <sbouhdjeur.gagny@gmail.com>
2025-12-16 22:11:39 +0100

    feat(auth): Enhance AuthInterceptor with logging and token management

--- app/src/main/java/com/groupe1/app_android/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/groupe1/app_android/ui/screens/HomeScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -32,6 +33,10 @@ fun HomeScreen(
     onTriggerFilterAd: () -> Unit,
     onItemClick: (Long) -> Unit
 ) {
+    LaunchedEffect(currentUser.id) {
+        listingsViewModel.getListingsFromRepo()
+    }
+
     // TODO: USE ME
     val remoteListings by listingsViewModel.remoteListings.collectAsState()
 
